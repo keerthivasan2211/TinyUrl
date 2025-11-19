caadd0af258b57930367e800f80aa32bd77f9031
Keerthi Vasan <155253910+keerthivasan2211@users.noreply.github.com>
2025-11-19 21:14:44 +0530

    redirectRoute
    Removed commented lines for cleaner code.

--- Backend/routes/redirectRoutes.js
+++ Backend/routes/redirectRoutes.js
@@ -1,9 +1,9 @@
-// routes/redirectRoutes.js
+
 const express = require("express");
 const router = express.Router();
 const { handleRedirect } = require("../Controllers/redirectController.js");
 
-// REDIRECT ROUTE (must come LAST)
+
 router.get("/:code", handleRedirect);
 
 module.exports = router;
